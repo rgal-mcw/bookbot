9bfe0316f76eeb8fb1f5fa0b9df7ebf7f19e0162
rgal-mcw <ryan.biostat@gmail.com>
2025-07-18 18:58:00 -0500

    Created BookBot to return letter counts of input book files

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 
-from stats import get_num_words, counting_chars
+import sys
+
+from stats import get_num_words, counting_chars, report_chars
+
+
 
 def main():
-    return print(counting_chars("books/frankenstein.txt"))
+
+    
+    if len(sys.argv) != 2:
+        raise Exception("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    return report_chars(sys.argv[1])
 
 main()
--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ def get_num_words(filepath):
         book = f.read()
         book_string = book.split()
         book_len = len(book_string)
-        return f"{book_len} words found in the document"
+        return f"{book_len}"
 
 def counting_chars(filepath):
     book_dict = {}
@@ -11,13 +11,35 @@ def counting_chars(filepath):
         book = f.read()
         book_lower = list(book.lower())
         unique_char = set(book_lower)
-
-        print(unique_char)
-
+        
         for i in unique_char:
             book_dict[i] = book_lower.count(i)
 
         return book_dict
         
 
+def sort_on(items):
+    return items["num"]
 
+
+def report_chars(filepath):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+
+    num_words = get_num_words(filepath)
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    
+    print("--------- Character Count -------")
+    
+    book_dict = counting_chars(filepath) 
+
+    letters = list()
+    for letter in book_dict:
+        baby_dict = {"letter":letter, "num":book_dict[letter]}
+        letters.append(baby_dict)
+    letters.sort(reverse=True, key=sort_on)
+
+    for i in letters:
+        if i["letter"].isalpha() == True:
+            print(f"{i["letter"]}: {i["num"]}")
